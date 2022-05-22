4dbb59d02e2174a1a55332677b472ebe2ddb2d0e
shlyapo <72437851+shlyapo@users.noreply.github.com>
2022-05-22 23:06:14 +0300

    Add files via upload

--- lab03/ex4.s
+++ lab03/ex4.s
@@ -2,7 +2,7 @@
 .globl recursive
 
 .data
-n: .word 8
+n: .word 11432
 m: .word 2
 
 .text
@@ -25,12 +25,53 @@ main:
 tester:
     # YOUR CODE HERE
     # вызвать итеративную и рекурсивные функции, сравнить ответ и вернуть результат, если совпал. иначе вернуть -1.
-	jalr zero, 0(ra)
+    addi sp, sp, -8
+    sw a0, 0(sp)
+    sw ra, 4(sp)
+    jal iterative
+    add t2, a0, x0 # t2 = a0
+    lw a2, 0(sp)
+    # addi sp, sp, 4
+    
+    addi a1, x0, 1 # a1 = 0 
+    addi a0, x0, 0 # a0 = 0 
+    jal recursive
+    add t1, a0, x0 # t1 = a0
+    addi a0, x0, 1 # a0 = 1
+    beq t2, t1, eq
+    addi a0, x0, -1 # a0 = -1
+    eq:
+	#jalr zero, 0(ra)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    jr ra
+
 
 iterative:
     # YOUR CODE HERE
-	jalr zero, 0(ra)
+    addi t0, x0, 1 # mask
+    addi s0, x0, 0 # rang
+    loop:
+    and t1, a0, t0
+    beq t1, x0, zzero # t1 == 0
+    addi s0, s0, 1
+    zzero:
+    slli t0, t0, 1
+    bne t0, x0, loop
+    add a0, s0, x0
+    jr ra
     
 recursive:
     # YOUR CODE HERE
-	jalr zero, 0(ra)
+    beq a1, x0, out # a1 - mask
+    and t1, a2, a1 # a2 - digit
+    beq t1, x0, zzzero # t1 == 0
+    addi a0, a0, 1
+    zzzero:
+    slli a1, a1, 1
+    
+    beq x0, x0, recursive
+   
+    out:
+    add a0, s0, x0
+    jr ra
